b602004a8ce050f1f161361ccdc3b883d77cd0be
aibekzhumabekov <aibek.zhumabekov@proton.me>
2025-11-28 11:57:45 -0600

    Refactor homepage structure for improved readability by adjusting indentation and formatting.

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,16 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div id="maincontent">
-   <h1>Hello, World.</h1>
-   <p>Software Engineer, based in the U.S.</p>
-   <p>I run on caffeine and curiosity.</p>
-   <p>I write code in English and JavaScript.</p>
-   <p>I build things that (sometimes) work.</p>
-   <p>If you’re in tech, let’s connect.</p>
-      <p>
-        <br />
-        <Link href="/contact">Contact</Link>
-      </p>
+    <h1>Hello, World.</h1>
+    <p>Software Engineer, based in the U.S.</p>
+    <p>I run on caffeine and curiosity.</p>
+    <p>I write code in English and JavaScript.</p>
+    <p>I build things that (sometimes) work.</p>
+    <p>If you’re in tech, let’s connect.</p>
+        <p>
+          <br />
+          <Link href="/contact">Contact</Link>
+        </p>
     </div>
   );
 }
